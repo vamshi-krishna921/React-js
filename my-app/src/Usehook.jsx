fde598dfc25ad7e04e22fbf78ddea7646c7d9c7b
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-08-15 07:18:03 +0530

    Image slider using useState

--- my-app/src/Usehook.jsx
+++ my-app/src/Usehook.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa6";
 
 //* Begineer useState
 // function Usehook() {
@@ -164,7 +165,7 @@ import { useState } from "react";
 //         setValue(() => {
 //           return value.map(item => {
 //             return item + 1;
-            
+
 //           })
 //         })
 //       }}
@@ -210,7 +211,7 @@ import { useState } from "react";
 //         setValue(() => {
 //           return value.map(item => {
 //             return item + 1;
-            
+
 //           })
 //         })
 //       }}
@@ -223,42 +224,72 @@ import { useState } from "react";
 // }
 
 //* Changigng objects in array using useHook
-function Usehook() {
-  const [value, setValue] = useState([
-    {
-      name : "Ganesh",
-      age: 20,
-    },
-     {
-      name : "Jai",
-      age: 23,
-    },
-     {
-      name : "Hanuman",
-      age: 22
-    }
-  ]);
+// function Usehook() {
+//   const [value, setValue] = useState([
+//     {
+//       name : "Ganesh",
+//       age: 20,
+//     },
+//      {
+//       name : "Jai",
+//       age: 23,
+//     },
+//      {
+//       name : "Hanuman",
+//       age: 22
+//     }
+//   ]);
 
+//   return (
+//     <div className="w-[10%] h-[35dvh] rounded bg-zinc-100 flex flex-col justify-center items-center gap-4 shadow shadow-zinc-200">
+//       <p className="text-2xl font-light text-black">
+//        {value.map((item,index) => {
+//         return <div key={index}>
+//           <p>{item.name}</p>
+//           <p>{item.age}</p>
+//         </div>
+//        })}
+//       </p>
+//       <button onClick={() => {
+//         setValue(prev =>{ return prev.map((item) => {
+//         return item.name === "Jai" ? ({name: "Jai", age: 25}) : item
+//         } )})
+//       }}
+//         className="px-4.5 py-2.5 bg-sky-400 text-xl rounded hover:transform-translate hover:-translate-y-0.5 duration-100 hover:shadow shadow-sky-900"
+//       >
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }
+
+//Todo Image slider using useState hook
+function Usehook() {
+  const [val, setVal] = useState(false);
   return (
-    <div className="w-[10%] h-[35dvh] rounded bg-zinc-100 flex flex-col justify-center items-center gap-4 shadow shadow-zinc-200">
-      <p className="text-2xl font-light text-black">
-       {value.map((item) => {
-        return <div>
-          <p>{item.name}</p>
-          <p>{item.age}</p>
-        </div>
-       })}
-      </p>
-      <button onClick={() => {
-        setValue(() =>{ return value.map((item) => {
-        return item.name === "Jai" ? ({name: "Jai", age: 25}) : item
-        } )})
-      }}
-        className="px-4.5 py-2.5 bg-sky-400 text-xl rounded hover:transform-translate hover:-translate-y-0.5 duration-100 hover:shadow shadow-sky-900"
+    <div className="relative w-[30vw] h-[40vh] bg-zinc-500 rounded-md flex overflow-hidden">
+      <img
+        src="https://images.unsplash.com/photo-1514897575457-c4db467cf78e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+        className={`w-full h-full object-cover shrink-0 transition-transform ${!val? "-translate-x-[0%]" : "-translate-x-[100%]"}`}
+      />
+      <img
+        src="https://images.unsplash.com/photo-1503416997304-7f8bf166c121?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+        className={`w-full h-full object-cover shrink-0 transition-transform ${!val? "-translate-x-[0%]" : "-translate-x-[100%]"} `}
+      />
+      <div
+        onClick={() =>
+          setVal(() => {
+            return !val;
+          })
+        }
+        className="absolute bottom-2 left-[40%] size-15 rounded-full bg-zinc-300/40 flex justify-center items-center"
       >
-        Click me
-      </button>
+        <FaArrowRight />
+      </div>
     </div>
   );
 }
+
 export default Usehook;
