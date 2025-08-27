3b2a3d7003a1daed27c3c4d64c858d62130784b7
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-08-27 11:05:55 +0530

    TanStack-Query

---
+++ Learn-tanStack/src/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Products } from "./components/Products";
+import { Services } from "./components/Services";
+
+export const Navbar = () => {
+  return (
+    <>
+      <div className="w-full h-[10vh] bg-gray-200 flex justify-center items-center gap-6">
+        <NavLink
+          to="/"
+          className={({ isActive }) => 
+            isActive
+              ? "text-blue-500 text-md font-bold"
+              : "text-zinc-500 text-md hover:text-zinc-700"
+          }
+          element={<Home />}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/products"
+          className={({ isActive }) => 
+            isActive
+              ? "text-blue-500 text-md font-bold"
+              : "text-zinc-500 text-md hover:text-zinc-700"
+          }
+          element={<Products />}
+        >
+          Products
+        </NavLink>
+        <NavLink
+          to="/services"
+          className={({ isActive }) => 
+            isActive
+              ? "text-blue-500 text-md font-bold"
+              : "text-zinc-500 text-md hover:text-zinc-700"
+          }
+          element={<Services />}
+        >
+          Services
+        </NavLink>
+      </div>
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </>
+  );
+};
+export default Navbar;
---
+++ Learn-tanStack/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./Query/QueryClinet.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
+);
