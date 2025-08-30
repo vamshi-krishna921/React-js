adf1d60075cb5b661b54a5df163b49c00b59d32c
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-08-30 20:13:42 +0530

    mutation data delete in api

--- Learn-tanStack/src/components/Products.jsx
+++ Learn-tanStack/src/components/Products.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router-dom";
 import ProductDetails from "./ProductDetails";
-import Api from  "../Utils/Api";
-import { useQuery } from "@tanstack/react-query";
+import Api from "../Utils/Api";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
 export const Products = () => {
@@ -19,15 +19,28 @@ export const Products = () => {
   const { data, isError, isLoading, refetch } = useQuery({
     queryKey: ["products", page],
     queryFn: async () => {
-      const res = await Api.get(
-        `/posts?_start=${
-          (page - 1) * 10
-        }&_limit=10`
-      );
+      const res = await Api.get(`/posts?_start=${(page - 1) * 10}&_limit=10`);
       return res.data;
     },
     keepPreviousData: true,
   });
+
+  //* useQueryClient Example
+  const queryClient = useQueryClient();
+
+  //* useMutation Example
+  const deleteProduct = useMutation({
+    mutationFn: async (id) => {
+      await Api.delete(`/posts/${id}`);
+    },
+    onSuccess: (_, id) => {
+      queryClient.setQueryData(["products", page], (curElem) =>
+        curElem.filter((product) => product.id !== id)
+      );
+      // queryClient.invalidateQueries({ queryKey: ["products"] });
+    },
+  });
+
   if (isLoading)
     return (
       <h1 className="text-center font-bold text-green-300 ">Loading...</h1>
@@ -39,8 +52,8 @@ export const Products = () => {
       <div className="w-full flex justify-center items-center gap-6 flex-wrap p-6">
         {data.map((product) => {
           return (
-            <Link to={`/products/${product.id}`} key={product.id}>
-              <div className="w-82 h-126 bg-green-300 flex flex-col justify-start items-center gap-2 p-2 rounded-md cursor-pointer overflow-hidden">
+            <Link className="cursor-default" to={`/products/${product.id}`} key={product.id}>
+              <div className="w-82 h-126  bg-green-300 flex flex-col justify-start items-center gap-2 p-2 rounded-md overflow-hidden">
                 <div className="w-full shrink-0 h-60 bg-gray-200 flex justify-center rounded-md items-center overflow-hidden">
                   <img
                     src={product.image}
@@ -51,6 +64,15 @@ export const Products = () => {
                 <h1 className="text-xl font-bold">{product.title}</h1>
                 <p className="text-md">{product.price}</p>
                 <p className="text-md">{product.description}</p>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    deleteProduct.mutate(product.id);
+                  }}
+                  className="px-4 py-2 cursor-pointer bg-red-400 shadow shadow-red-600 rounded-2xl"
+                >
+                  Delete
+                </button>
               </div>
             </Link>
           );
@@ -60,7 +82,7 @@ export const Products = () => {
         <button
           disabled={page === 1}
           onClick={() => setPage((prev) => prev - 1)}
-          className={`px-3 py-2 ${
+          className={`px-3 py-2 cursor-pointer ${
             page === 1
               ? "bg-gray-300 shadow-gray-600"
               : "bg-green-300 shadow-green-600"
@@ -70,8 +92,9 @@ export const Products = () => {
         </button>
         <h1>{page}</h1>
         <button
+          disabled={data.length < 10}
           onClick={() => setPage((prev) => prev + 1)}
-          className="px-3 py-2 bg-green-300 shadow shadow-green-600"
+          className="px-3 py-2 cursor-pointer bg-green-300 shadow shadow-green-600"
         >
           Next
         </button>
