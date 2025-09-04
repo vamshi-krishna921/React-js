3000bb78585e1b79b2e0326e8c2eb4a7368f091d
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-09-04 21:56:40 +0530

    RTK

--- Redux/src/Store.jsx
+++ Redux/src/Store.jsx
@@ -1,46 +1,90 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import {thunk} from 'redux-thunk';
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "@redux-devtools/extension";
+// import {thunk} from 'redux-thunk';
 import axios from "axios";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+//*Redux no toolkit version
 
-export const ADD_TASK = "task/add";
-export const REMOVE_TASK = "task/remove";
-export const FETCH_TASKS = "task/fetch";
-const initialState = {
-  tasks: [],
-};
-const taskReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TASK:
-      return { ...state, tasks: [...state.tasks, action.payload] };
-    case REMOVE_TASK:
-      return {
-        ...state,
-        tasks: state.tasks.filter((_, index) => index !== action.payload),
-      };
-    case FETCH_TASKS:
-      return { ...state, tasks: [...state.tasks, ...action.payload] };
+// export const ADD_TASK = "task/add";
+// export const REMOVE_TASK = "task/remove";
+// export const FETCH_TASKS = "task/fetch";
+// const initialState = {
+//   tasks: [],
+// };
+// const taskReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_TASK:
+//       return { ...state, tasks: [...state.tasks, action.payload] };
+//     case REMOVE_TASK:
+//       return {
+//         ...state,
+//         tasks: state.tasks.filter((_, index) => index !== action.payload),
+//       };
+//     case FETCH_TASKS:
+//       return { ...state, tasks: [...state.tasks, ...action.payload] };
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
 
-export const store = createStore(
-  taskReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+// export const store = createStore(
+//   taskReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
 
-export const fetchTasks = () => {
-  return async (dispatch) => {
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts?_limit=5"
-      );
-      const tasks = response.data;
-      dispatch({ type: FETCH_TASKS, payload: tasks.map((task) => task.title) });
-    } catch (error) {
-      console.error("Error fetching tasks:", error);
+// export const fetchTasks = () => {
+//   return async (dispatch) => {
+//     try {
+//       const response = await axios.get(
+//         "https://jsonplaceholder.typicode.com/posts?_limit=5"
+//       );
+//       const tasks = response.data;
+//       dispatch({ type: FETCH_TASKS, payload: tasks.map((task) => task.title) });
+//     } catch (error) {
+//       console.error("Error fetching tasks:", error);
+//     }
+//   };
+// };
+
+//* Redux toolkit version
+
+//* creaete slice
+const taskReducer = createSlice({
+  name : "tasks",
+  initialState : ["Learn Redux", "Learn React"],
+  reducers : {
+    addTask : (state, action) => {
+      state.push(action.payload)
+    },
+    removeTask : (state, action) => {
+      state.splice(action.payload, 1)
+    },
+    fetchTasks : (state, action) => {
+      return [...state, ...action.payload]
     }
-  };
-};
+  }
+})
+console.log(taskReducer);
+
+export const {addTask, removeTask, fetchTasks} = taskReducer.actions
+
+export const store = configureStore({
+  reducer : {
+    tasks: taskReducer.reducer,
+  },
+})
+
+// export const fetchTasks = () => {
+//   return async (dispatch) => {
+//     try {
+//       const response = await axios.get(
+//         "https://jsonplaceholder.typicode.com/posts?_limit=5"
+//       );
+//       const tasks = response.data;
+//       dispatch({ type: FETCH_TASKS, payload: tasks.map((task) => task.title) });
+//     } catch (error) {
+//       console.error("Error fetching tasks:", error);
+//     }
+//   };
+// };
--- Redux/src/Todo.jsx
+++ Redux/src/Todo.jsx
@@ -1,7 +1,77 @@
 import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { useSelector, useDispatch } from "react-redux";
-import { ADD_TASK, REMOVE_TASK, fetchTasks} from "./Store";
+// import { ADD_TASK, REMOVE_TASK, fetchTasks} from "./Store";
+import { addTask, removeTask, fetchTasks } from "./store";
+
+
+//* Redux no toolkit version
+
+// function Todo() {
+//   const state = useSelector((state) => state.tasks);
+//   const [task, setTask] = useState("");
+//   const dispatch = useDispatch();
+//   //* Add task
+//   function HandleAddTask(e) {
+//     e.preventDefault();
+//     if (task.trim() === "") return;
+//     dispatch({ type: ADD_TASK, payload: task });
+//     setTask("");
+//   }
+//   //* Remove task
+//   function HandleRemoveTask(index){
+//     dispatch({type: REMOVE_TASK, payload: index})
+//   }
+
+//   //* Fetch Tasks from API
+//   function handleFetchTasks() {
+//     dispatch(fetchTasks());
+//   }
+//   return (
+//     <div className="w-[40%] min-h-1/2 bg-gray-100 shadow-lg rounded-lg flex flex-col items-center justify-start gap-5 px-6 py-1.5">
+//       <h1 className="text-4xl font-bold text-blue-600">Todo App</h1>
+//       <div className="w-full h-10 flex justify-center items-center gap-3.5">
+//         <form
+//           onSubmit={HandleAddTask}
+//           className="w-full h-10 flex justify-center items-center gap-3.5"
+//         >
+//           <input
+//             type="text"
+//             value={task}
+//             placeholder="Add your task"
+//             className="border-2 rounded-lg border-blue-100 px-3 py-2 focus:outline-blue-400 w-[80%]"
+//             onChange={(e) => setTask(e.target.value)}
+//           />
+//           <input
+//             type="submit"
+//             // value="Add Task"
+//             value="Fetch Tasks"
+//             onClick={() => handleFetchTasks()}
+//             className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+//           />
+//         </form>
+//       </div>
+//       <div className="w-full flex flex-col gap-3.5">
+//         {state.map((curTask, index) => (
+//           <div
+//             key={index}
+//             className="w-[95%] h-[10%] bg-gray-200 shadow shadow-gray-400 hover:bg-blue-500 hover:text-white flex items-center justify-between rounded-lg font-[500] px-5 py-4"
+//           >
+//             <p className="text-lg">{index + 1}</p>
+//             <p className="text-lg">{curTask}</p>
+//             <p className="text-xl cursor-pointer hover:text-red-600">
+//               <MdDelete onClick={() => HandleRemoveTask(index)}/>
+//             </p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Todo;
+
+//* Redux toolkit version
 
 function Todo() {
   const state = useSelector((state) => state.tasks);
@@ -11,12 +81,12 @@ function Todo() {
   function HandleAddTask(e) {
     e.preventDefault();
     if (task.trim() === "") return;
-    dispatch({ type: ADD_TASK, payload: task });
+    dispatch(addTask(task));
     setTask("");
   }
   //* Remove task
   function HandleRemoveTask(index){
-    dispatch({type: REMOVE_TASK, payload: index})
+    dispatch(removeTask(index))
   }
 
   //* Fetch Tasks from API
@@ -38,11 +108,9 @@ function Todo() {
             className="border-2 rounded-lg border-blue-100 px-3 py-2 focus:outline-blue-400 w-[80%]"
             onChange={(e) => setTask(e.target.value)}
           />
-          <input
+           <input
             type="submit"
-            // value="Add Task"
-            value="Fetch Tasks"
-            onClick={() => handleFetchTasks()}
+            value="Add Task"
             className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
           />
         </form>
